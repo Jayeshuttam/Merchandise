efe3454b62a65925de74b6ae86ba2c9957274b77
Jayeshuttam <38659267+Jayeshuttam@users.noreply.github.com>
2020-08-22 19:21:41 -0400

    first day

---
+++ index.php
@@ -0,0 +1,27 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>First PHP</title>
+</head>
+
+<body>
+<?php
+echo 'Hello World';
+$e = 'aiufbaioc';
+$f = 10.8;
+function test()
+{
+    echo '<br> Bye Byee World';
+}
+test();
+$x = 25;
+echo 'The value of is  $x';
+// echo $GLOBALS[0];
+var_dump($e);
+
+?>
+</body>
+
+</html>
