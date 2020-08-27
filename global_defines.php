1f86c664e096a915362ee2724939097eff604221
Jayeshuttam <38659267+Jayeshuttam@users.noreply.github.com>
2020-08-27 18:24:29 -0400

    second day first commit

---
+++ global_defines.php
@@ -0,0 +1,17 @@
+<?php
+/*
+ * this file contains all project constants
+ */
+
+ //WEBPAGE INFO
+define('WEB_SITE_NAME', 'ElectricScooters.com');
+define('PAGE_DEFAULT_TITLE', 'Welcome to ElectricScooters.com');
+define('PAGE_DEFAULT_DESCRIPTION', 'Electric Scooters.com has the widest collections of scooters 
+and bicycles ,new ,used in Montreal');
+define('PAGE_AUTHOR', 'Jayesh Uttam');
+define('ADMIN_EMAIL', 'ITBoss@electricScooters.com');
+
+//COMPANY INFO
+define('COMPANY_NAME', 'Electric Scooters');
+define('COMPANY_PHONE', '514-790-8389');
+define('COMPANY_EMAIL', 'serviceScooter@electricScooter.com');
---
+++ web_page.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Class WebPAge is used to output the HTML\CSS page.
+ */
+require_once 'global_defines.php';
+class Webpage
+{
+    public $title = PAGE_DEFAULT_TITLE;
+    public $description = '';
+    public $author = 'Jayesh Uttam';
+    public $lang = 'en-CA';
+    public $icon = 'my-icon.jpg';
+    public $css = 'css\styless.css';
+    /// public $content = '<h2>Welcome to Electric scooters.com </h2>';
+    public $content = '';
+
+    /**
+     * the constructor.
+     */
+    public function __construct()
+    {
+    }
+
+    public function render()
+    {
+        if ($this->content == '') {
+            //errr,,for checking if there is content in the page
+            http_response_code(500);
+
+            //  mail(ADMIN_EMAIL, 'Error in the web page', 'No content set in function render()');
+            die('Sorry we have a problem !!');
+        } else {
+            ?>
+<!DOCTYpe html>
+<html lang="<?=$this->lang; ?>">
+
+<head>
+    <meta charset="utf-8">
+    <title><?=$this->title; ?>
+    </title>
+    <meta name='description' value="<?=$this->description; ?>">
+    <meta name="author" value="<?=$this->author; ?>">
+    <link rel="icon" href="<?=$this->icon; ?>" type="image/jpeg">
+    <link rel="stylesheet" href="css/styless.css">
+</head>
+
+<body>
+    <header>
+        <img src="<?=$this->icon; ?>"
+            alt="<?=COMPANY_NAME; ?>" width="20" height="50">
+        <h1><?=WEB_SITE_NAME; ?>
+        </h1>
+    </header>
+    <nav>
+        <h1>THIS IS THE NAV BAR</h1>
+    </nav>
+    <main>
+        <?="$this->content"; ?>
+    </main>
+</body>
+
+</html>
+
+<?php
+die(); //Stop program
+        }
+    }
+}
